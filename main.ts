fd99659c108364788b024ec3bb839be5e37b4dd7
Wilmer Setterholm <144359249+setterholm@users.noreply.github.com>
2023-09-13 15:53:42 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,94 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(antalSteg)
-})
-input.onButtonPressed(Button.AB, function () {
-	
+    if (emojichosen == false) {
+        chooseemoji()
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    emoji += 1
-    if (emoji == 1) {
-        basic.showIcon(IconNames.Scissors)
-    } else if (emoji == 2) {
-        basic.showIcon(IconNames.Heart)
-    } else if (emoji == 3) {
-        basic.showIcon(IconNames.Square)
-    } else if (emoji == 4) {
-        basic.showIcon(IconNames.Skull)
-    } else {
-        emoji = 1
-        basic.showIcon(IconNames.Scissors)
+    if (emojichosen == false) {
+        if (emojinumber == 0 && emoji_0_taken == true) {
+            basic.showIcon(IconNames.No)
+        } else {
+            if (emojinumber == 1 && emoji_1_taken == true) {
+                basic.showIcon(IconNames.No)
+            } else {
+                if (emojinumber == 2 && emoji_2_taken == true) {
+                    basic.showIcon(IconNames.No)
+                } else {
+                    if (emojinumber == 3 && emoji_3_taken == true) {
+                        basic.showIcon(IconNames.No)
+                    } else {
+                        emojichosen = true
+                    }
+                }
+            }
+        }
+    }
+    if (emojichosen == true) {
+        if (emojinumber == 0) {
+            radio.sendValue("emoji 0", steps)
+        } else if (emojinumber == 1) {
+            radio.sendValue("emoji 1", steps)
+        } else if (emojinumber == 2) {
+            radio.sendValue("emoji 2", steps)
+        } else if (emojinumber == 3) {
+            radio.sendValue("emoji 3", steps)
+        }
     }
 })
 input.onGesture(Gesture.Shake, function () {
-    antalSteg += 1
+    if (emojichosen == true) {
+        steps += 1
+    }
 })
-let emoji = 0
-let antalSteg = ""
-basic.showIcon(IconNames.Happy)
+radio.onReceivedValue(function (name, value) {
+    if (name == "emoji 0") {
+        emoji_0 = value
+        emoji_0_taken = true
+    } else if (name == "emoji 1") {
+        emoji_1 = value
+        emoji_1_taken = true
+    } else if (name == "emoji 2") {
+        emoji_2 = value
+        emoji_2_taken = true
+    } else if (name == "emoji 3") {
+        emoji_3 = value
+        emoji_3_taken = true
+    }
+})
+function chooseemoji () {
+    if (emojinumber == 3) {
+        emojinumber = 0
+    } else {
+        emojinumber += 1
+    }
+    if (emojinumber == 0) {
+        basic.showIcon(IconNames.Happy)
+    } else if (emojinumber == 1) {
+        basic.showIcon(IconNames.Heart)
+    } else if (emojinumber == 2) {
+        basic.showIcon(IconNames.Sad)
+    } else if (emojinumber == 3) {
+        basic.showIcon(IconNames.Duck)
+    }
+}
+let emoji_3 = 0
+let emoji_2 = 0
+let emoji_1 = 0
+let emoji_0 = 0
+let emoji_3_taken = false
+let emoji_2_taken = false
+let emoji_1_taken = false
+let emoji_0_taken = false
+let emojinumber = 0
+let emojichosen = false
+let steps = 0
 radio.setGroup(40)
+steps = 0
+emojichosen = false
+emojinumber = 0
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    radio.sendValue(antalSteg, 0)
+    if (emojichosen == true) {
+        basic.showNumber(steps)
+    }
 })
